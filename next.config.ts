d5d01904464509a3205f2d2454e971026c2dcdc4
Nicholas Ricketts <nri@vt.edu>
2025-10-03 01:21:49 -0400

    Another attempted fix at github pages hosting.

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,12 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
+// const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   // Enables static site export
   output: 'export',
-  assetPrefix: isProd ? './' : '', 
+  basePath: '/professional-website',
+  assetPrefix: '/professional-website/',
 };
 
 export default nextConfig;
