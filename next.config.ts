c4f3d7e2d2326167d078742ef800d61a9e2b046a
Nicholas Ricketts <nri@vt.edu>
2025-10-03 01:47:10 -0400

    I'm using vercel, github pages is clearly not meant for this. I'm trying to put a square in a circle hole.

--- next.config.ts
+++ next.config.ts
@@ -3,29 +3,6 @@ import type { NextConfig } from "next";
 // const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
-  /**
-   * Enable static exports.
-   *
-   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
-   */
-  output: 'export',
-
-  /**
-   * Set base path. This is the slug of your GitHub repository.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-   */
-  basePath: '/professional-website',
-  
-  /**
-   * Disable server-based image optimization. Next.js does not support
-   * dynamic features with static exports.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
-   */
-  images: {
-    unoptimized: true
-  }
 };
 
 export default nextConfig;
